f764d2f70a8cd203f66f1e6ad802ff8af3e66ca7
Jose David <lujan9495@gmail.com>
2025-11-08 11:19:45 -0500

    Implement should_continue function to manage LLM tool call logic and rename MessageState to MessagesState

---
+++ src/logic.py
@@ -0,0 +1,19 @@
+from langgraph.graph import END 
+from langchain_core.messages import AIMessage
+from .state import MessagesState
+
+def should_continue(state: MessagesState) -> str:
+  """Decide if we should continue the loop or stop based upon whether the LLM made a tool call
+  
+  Can return ["tool_node", END]
+  """
+  
+  messages = state["messages"]
+  last_message = messages[-1]
+  
+  # If the LLM makes a tool call, then perform an action
+  if isinstance(last_message, AIMessage) and last_message.tool_calls:
+    return "tool_node"
+  
+  # Otherwise, we are done
+  return END
--- src/state.py
+++ src/state.py
@@ -2,6 +2,6 @@ from langchain.messages import AnyMessage
 from typing_extensions import TypedDict, Annotated
 import operator 
 
-class MessageState(TypedDict):
+class MessagesState(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
     llm_calls: int
